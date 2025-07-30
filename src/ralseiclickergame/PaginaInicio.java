952b0b75ddce811ab1edf2bb4386a51305225bff
Keniak1-G9 <Keniak1-G9@hotmail.com>
2025-07-29 21:24:09 -0500

    Se creó PaginaInicio, (a mitad de completar)
    - Aún falta más cosas que añadir, pero es una actualización a mitad de trabajo por si las moscas
    - Muestra la universidad, facultad, carrera, grupo, profesor, integrantes, y fecha de entrega
    - Usa la fuente "determination" que está presente en el juego
    - Usa fondo negro con letras en blanco, similar al juego homenajeado y sea estéticamente coherente con el resto del juego
    - Falta: agregar los logos de FISC y UTP; el nombre de la UTP en una fuente más grande; El botón "Jugar" y al realizar click traiga la ventana de juego; Que esta página de inicio sea lo que se inicia al ejecutar el programa.

--- src/ralseiclickergame/PaginaInicio.java
+++ src/ralseiclickergame/PaginaInicio.java
@@ -2,7 +2,50 @@ package ralseiclickergame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class PaginaInicio extends JFrame {
 
+    private JPanel panelPaginaInicio;
+
+    public PaginaInicio()
+    {
+        setTitle("Clicker Ralsei");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(975, 720);
+        setLocationRelativeTo(null);
+        setBackground(new Color(15, 12, 25));
+        setVisible(true);
+
+
+    }
+
+
+
+    private void asignarFuenteParaLabelenPanel(Container container, Font font) {
+        for (Component comp : container.getComponents()) {
+            if (comp instanceof JLabel) {
+                comp.setFont(font);
+            } else if (comp instanceof Container) {
+                // Recursivamente llama a todos los contenedores
+                asignarFuenteParaLabelenPanel((Container) comp, font);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PaginaInicio paginaInicio = new PaginaInicio();
+
+        try {
+            File fuenteEstilo = new File("src/resources/determination.ttf");
+            Font fuente = Font.createFont(Font.TRUETYPE_FONT, fuenteEstilo).deriveFont(30f);
+            paginaInicio.asignarFuenteParaLabelenPanel(paginaInicio.panelPaginaInicio, fuente);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        paginaInicio.setContentPane(paginaInicio.panelPaginaInicio);
+    }
 }
